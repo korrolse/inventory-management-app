aa112fabe7b3fd16606ae92b07973f7eed933fa7
Sofia <shatunova.sofiya@gmail.com>
2026-08-13 15:47:23 +0200

    Add Express server and PostgreSQL connection

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,14 @@
 const express = require('express');
+const pool = require('./db');
+
 const app = express();
 
-app.get('/products', (req, res) => {
-    res.json([
-        {
-            id: 1,
-            name: 'Mouse',
-            quantity: 10,
-            price: 25
-        },
-        {
-            id: 2,
-            name: 'Keyboard',
-            quantity: 3,
-            price: 50
-        }
-    ]);
-});
+app.use(express.json());
+
+app.get('/products', async (req, res) => {
+    const result = await pool.query('SELECT * FROM products');
+    res.json(result.rows);
+})
 
 app.listen(4000, () => {
     console.log('Server is running on port 4000')
